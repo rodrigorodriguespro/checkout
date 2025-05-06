4f57ff54c8fa184dc5a8cc5d80f022e6b7e1f3ef
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2025-05-06 20:17:09 -0300

    ✨ feat: Adiciona módulos de configuração e autenticação
    Implementa o módulo de configuração global e integra suporte ao Bull para gerenciamento de filas. Adiciona módulos de pagamento e autenticação ao aplicativo, melhorando a estrutura e a funcionalidade da aplicação.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PaymentModule } from './payment/payment.module';
+import { AuthModule } from './auth/auth.module';
+import { BullConfigService } from './queue/bull-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['../../.env'] }),
+    BullModule.forRootAsync({
+      useClass: BullConfigService,
+    }),
+    PaymentModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
